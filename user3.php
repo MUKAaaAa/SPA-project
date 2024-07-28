12cc5285bb12be70fd398aa28194e241c42637f7
mukarram niyas <mukarramniyas1212@gmail.com>
2024-07-28 22:28:26 +0530

    first commend

---
+++ user3.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> users </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.23/dist/css/uikit.min.css" />
+</head>
+<body>
+<section class="home" id="home">
+    <nav class="main-navbar uk-navbar-container">
+    <div class="uk-container">
+         <a class=" back-button uk-navbar-item uk-logo " href ="home.php " >BACK</a>
+           
+    </div>
+</nav>
+<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+    <div class="uk-card-media-left uk-cover-container">
+        <img src="https://img.freepik.com/free-photo/blue-color-squares-arrangement-view_23-2149345128.jpg?size=626&ext=jpg&ga=GA1.1.1930204137.1711497600&semt=ais" alt="" uk-cover>
+        <canvas width="600" height="400"></canvas>
+    </div>
+    <div>
+        <div class="uk-card-body">
+            <h2 class="uk-card-title">First name :Richard </h3>
+            <h2 class="uk-card-title">Last name:Henrson</h3>
+            <h2 class="uk-card-title">Email: richard123@gmail.com</h3>
+            
+        </div>
+    </div>
+</div>
+
+
+</body>
